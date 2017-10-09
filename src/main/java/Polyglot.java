1811092351e883a8a1a4bbea7b88af580062b1b4
Julian Thome <julian.thome@uni.lu>
2017-10-09 23:44:03 +0200

    header

--- src/main/java/Polyglot.java
+++ src/main/java/Polyglot.java
@@ -1,3 +1,20 @@
+/*
+ * polyglot - translate constraints in between different formats
+ * Copyright (C) 2017 Julian Thome <julian.thome.de@gmail.com>
+ *
+ * polyglot is licensed under the EUPL, Version 1.1 or – as soon
+ * they will be approved by the European Commission - subsequent versions of the
+ * EUPL (the "Licence"); You may not use this work except in compliance with the
+ * Licence. You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/sites/default/files/eupl1.1.-licence-en_0.pdf
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied.  See the Licence for the
+ * specific language governing permissions and limitations under the Licence.
+ */
+
 import com.github.hycos.cnetwork.core.graph.ConstraintNetworkBuilder;
 import com.github.hycos.cnetwork.exception.EUFInconsistencyException;
 import com.github.hycos.cnetworkparser.core.ConstraintNetworkParser;
